e9e84ef7db492030d69a16c311da7b2767dc33e5
Riaan Taveras <riaantaveras@gmail.com>
2019-12-03 22:06:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,9 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  our_Hero = "It's Dangerous To Go alone! Take This.\n"
+  puts our_Hero
+end
+
